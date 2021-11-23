23458d7a6e6d369776fc7de1763e1a08e20bdd42
FrezaRo <tochkaperm2010@gmail.com>
2021-11-23 14:34:36 +0500

    lab 9_2 remember questions

--- Lab9.py
+++ Lab9.py
@@ -1,5 +1,15 @@
 from random import choice
+
 question = input('Напиши свой вопрос: ')
+questions_dict = {}
+
 while question != 'нет':
-    print (choice(['Да', 'Нет']))
+    if question in questions_dict.keys():
+        print (questions_dict[question])
+    else:
+        answer = choice(['Да', 'Нет'])
+        questions_dict[question] = answer
+        print (answer)
+    
     question = input('Напиши следующий вопрос или "нет" для остановки: ')
+
